9ba6725c1a445813f39e84004f93f0e6df3031bb
froop <frooprogrammer@gmail.com>
2013-05-02 12:25:25 +0900

    delete comment

--- WebContent/js/example.js
+++ WebContent/js/example.js
@@ -74,28 +74,5 @@
 			model: new Editor(),
 			items : new Items()
 		});
-
-//		var obj = new Backbone.Model();
-//		var obj2 = new Backbone.Model({name: "Kenichiro", age: 30});
-//		var objs = new Backbone.Collection([obj, obj2]);
-//
-//		obj.set({name: "Murata"});
-//		obj.set({age: 20});
-//
-//		console.log("obj: " + JSON.stringify(obj));
-//		console.log("obj.name: " + obj.get("name"));
-//
-//		console.log("objs: " + JSON.stringify(objs));
-//		 
-//		console.log("objs.get(cid): " + JSON.stringify(objs.get("c1")));
-//		console.log("objs.at(index): " + JSON.stringify(objs.at(0)));
-//
-//		// add
-//		objs.add(new Backbone.Model({name: "Acroquest", age: 20}));
-//		objs.add(new Backbone.Model({name: "Technology", age: 10}));
-//
-//		// length
-//		console.log("objs.length: " + objs.length);
-//		console.log("objs: " + JSON.stringify(objs));
 	});
 })(jQuery);
