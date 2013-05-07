22e1dbbf803dbddc1f778640ea1e404232462f65
froop <frooprogrammer@gmail.com>
2013-05-07 10:33:18 +0900

    extract example/models.js

--- WebContent/js/example/example.js
+++ WebContent/js/example/example.js
@@ -1,28 +1,7 @@
-/*global jQuery, Backbone, _ */
-
-var Example = new function () {};
+/*global jQuery, _, Backbone */
 
 (function ($) {
 	"use strict";
-
-	Example.Editor = Backbone.Model.extend({
-		defaults: {
-			text1: ""
-		},
-		initialize: function (attrs, options) {
-		},
-		validate: function (attrs) {
-			if (_.isEmpty(attrs.text1)) {
-				return "required";
-			}
-			if (_.isEmpty(this.changedAttributes(attrs))) {
-				return "not change";
-			}
-		},
-		setText1: function (value) {
-			this.set("text1", value, {validate: true});
-		}
-	});
 
 	var Item = Backbone.Model.extend({});
 	var Items = Backbone.Collection.extend({
