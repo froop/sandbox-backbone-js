67f2227b1c91a300b6a9e9ebea2b6c8746d3abb5
froop <frooprogrammer@gmail.com>
2013-05-08 13:49:49 +0900

    add test("editorView submit"

--- test/example/test-integrate.js
+++ test/example/test-integrate.js
@@ -1,6 +1,14 @@
 (function ($) {
 	module("Integrate", {
 		setup: function () {
+			new Example.AppView({
+				el: "#example",
+				editor: new Example.Editor(),
+				items: new Example.Items([], {
+					localStorage: new Backbone.LocalStorage("example-items")
+				})
+			});
+			$("#clear").click();
 		}
 	});
 
@@ -10,5 +18,20 @@
 		$("#add", $form).click();
 
 		equal($("#error", $form).text(), "required");
+		equal($("#count", $form).text(), 0);
+	});
+
+	test("editorView submit", function () {
+		var $form = $("#edit-form");
+		var $list = $("#list1");
+		$("input[name=text1]", $form).val("a");
+		$("#add", $form).click();
+		$("input[name=text1]", $form).val("b");
+		$("#add", $form).click();
+
+		equal($("#count", $form).text(), "2");
+		equal($("li", $list).length, 2);
+		equal($(".text1", $list).eq(0).text(), "a");
+		equal($(".text1", $list).eq(1).text(), "b");
 	});
 })(jQuery);
