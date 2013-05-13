3bb3b35fc380d97ec0404baaa0eb8868b898a097
froop <frooprogrammer@gmail.com>
2013-05-13 09:29:29 +0900

    update jquery.clonetemplate.js

--- WebContent/js/lib/jquery.clonetemplate.js
+++ WebContent/js/lib/jquery.clonetemplate.js
@@ -1,7 +1,7 @@
 /*
  * jquery.clonetemplate.js - jQuery plugin.
  *
- * Created by froop http://github.com/froop/sandbox-backbone-js
+ * Created by froop http://github.com/froop/jquery-clone-template
  * The MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 /*global jQuery */
@@ -9,16 +9,23 @@
 	"use strict";
 
 	/**
-	 * @param {Object} map className: value
+	 * @param {Object} map className / input[name]: value
 	 */
 	$.fn.cloneTemplate = function (map) {
 		var $template = this;
 		var $element = $template.clone();
 
-		$.each(map, function (className) {
-			var $field = $element.filter("." + className);
-			//TODO
-			$field.text(map[className]);
+		function setValue($fields, value) {
+			$fields.filter("input").val(value); //TODO checkbox, radio, select
+			$fields.not("input").text(value);
+		}
+
+		$.each(map, function (name) {
+			//TODO refactoring
+			setValue($element.filter("." + name), map[name]);
+			setValue($element.find("." + name), map[name]);
+			setValue($element.filter("input[name=" + name + "]"), map[name]);
+			setValue($element.find("input[name=" + name + "]"), map[name]);
 		});
 
 		return $element;
