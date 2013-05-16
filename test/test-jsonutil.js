96870bfefdec9fba593064edc69e10e7602366a6
froop <frooprogrammer@gmail.com>
2013-05-16 15:57:17 +0900

    test add JsonUtil.alias()

--- test/test-jsonutil.js
+++ test/test-jsonutil.js
@@ -4,7 +4,49 @@
 		}
 	});
 
-	test("test", function () {
-		ok(true);
+	test("alias frist", function () {
+		var result = JsonUtil.alias({
+			prop1: "value1",
+			prop2: "value2"
+		},
+		{
+			prop1: "alias1"
+		});
+
+		deepEqual(result, {
+			alias1: "value1",
+			prop2: "value2"
+		});
+	});
+
+	test("alias last", function () {
+		var result = JsonUtil.alias({
+			prop1: "value1",
+			prop2: "value2"
+		},
+		{
+			prop2: "alias2"
+		});
+
+		deepEqual(result, {
+			prop1: "value1",
+			alias2: "value2"
+		});
+	});
+
+	test("alias all", function () {
+		var result = JsonUtil.alias({
+			prop1: "value1",
+			prop2: "value2"
+		},
+		{
+			prop1: "alias1",
+			prop2: "alias2"
+		});
+
+		deepEqual(result, {
+			alias1: "value1",
+			alias2: "value2"
+		});
 	});
 })();
